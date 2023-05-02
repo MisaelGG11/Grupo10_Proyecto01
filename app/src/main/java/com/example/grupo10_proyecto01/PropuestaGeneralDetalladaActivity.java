673e61d0fc8f69d0605284489d5d7c0ef7dbd9f2
Alexander <hs19011@ues.edu.sv>
2023-05-02 16:29:18 -0600

    pantallas vacias alexander

---
+++ app/src/main/java/com/example/grupo10_proyecto01/PropuestaGeneralDetalladaActivity.java
@@ -0,0 +1,14 @@
+package com.example.grupo10_proyecto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PropuestaGeneralDetalladaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_propuesta_general_detallada);
+    }
+}
