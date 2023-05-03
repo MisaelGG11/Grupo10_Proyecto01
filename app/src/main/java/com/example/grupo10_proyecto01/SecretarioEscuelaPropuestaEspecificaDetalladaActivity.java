00a33d62e5a4b35fd0762b01e196c0b70ff045ab
Claudia María Argueta Caneza <91162449+ClaudiaArguetaCaneza@users.noreply.github.com>
2023-05-03 12:16:07 -0600

    Pantallas Vacias

---
+++ app/src/main/java/com/example/grupo10_proyecto01/SecretarioEscuelaPropuestaEspecificaDetalladaActivity.java
@@ -0,0 +1,14 @@
+package com.example.grupo10_proyecto01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecretarioEscuelaPropuestaEspecificaDetalladaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secretario_escuela_propuesta_especifica_detallada);
+    }
+}
